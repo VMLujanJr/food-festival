35fb5f154d73a162b14270b9ee1c6a46a6e1b517
VMLujanJr <vmlujanjr@outlook.com>
2022-07-02 23:01:30 -0500

    added new entries in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,17 @@ const webpack = require('webpack');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: '[name].bundle.js',
+        path: __dirname + '/dist'
+        // path: path.resolve(__dirname, 'dist'),
+        // filename: 'main.bundle.js'
     },
     // plugins for vendor code
     plugins: [
