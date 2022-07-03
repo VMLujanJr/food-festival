a835881017e7245400863d31f673741bd489a9f9
VMLujanJr <vmlujanjr@outlook.com>
2022-07-02 22:41:45 -0500

    modularizing code and separating concerns

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
     entry: './assets/js/script.js',
@@ -7,10 +8,15 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.bundle.js'
     },
+    // plugins for vendor code
     plugins: [
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery'
+        }),
+        new BundleAnalyzerPlugin({
+            // can also 'disable' the 'static' value
+            analyzerMode: 'static', // the report outputs to an HTML file in the dist folder
         })
     ],
     mode: 'development' // by default webpack wants to run in 'production' mode
