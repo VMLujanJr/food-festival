e35cf7515f4681fcaddc9f81126b628d86714a1d
VMLujanJr <vmlujanjr@outlook.com>
2022-07-02 23:44:31 -0500

    finished adding file-loader, image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,30 @@ module.exports = {
         // path: path.resolve(__dirname, 'dist'),
         // filename: 'main.bundle.js'
     },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name (file) {
+                                return '[path][name].[ext]'
+                            },
+                            publicPath: function(url) {
+                                return url.replace('../','/assets/')
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
+    },
     // plugins for vendor code
     plugins: [
         new webpack.ProvidePlugin({
